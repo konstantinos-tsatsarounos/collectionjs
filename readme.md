fe040d812e40c07a26af3fa5fec53d71f23a9de6
Konstantinos Tsatsarounos <konstantinos.tsatsarounos@gmail.com>
2017-01-12 02:01:01 +0200

    Update dependencies and configuration

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
-# CollectionJS
+# _`CollectionJS`_ - a javascript dictionary library
 
-A collection / dictionary list library for javascript.
+A collection / dictionary list library for javascript. It provides a way to create enhanced Arrays, with search capabilities. Introduces two additional types of Arrays, the List and the Dictionary.
--- tests/collection.test.js
+++ tests/collection.test.js
@@ -5,3 +5,15 @@
  * Date: 11/1/2017
  * Time: 11:12 μμ
  */
+
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+var should = require('chai').should();
+
+var activity = require('../src/collection');
+
+describe('List', function () {
+	it('a valid object should return true', function () {
+		expect(true).to.be.true;
+	});
+});
