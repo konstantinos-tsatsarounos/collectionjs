d858ea8159b114300d52007d72f64c4f42490c33
Konstantinos Tsatsarounos <konstantinos.tsatsarounos@gmail.com>
2017-01-14 04:38:59 +0200

    add the ability for users to use this module within node using just the module name

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Konstantinos Tsatsarounos<konstantinos.tsatsarounos@gmail.com>
+ * Project Name:
+ * Filename: index.js
+ * Date: 14/1/2017
+ * Time: 4:34 πμ
+ */
+
+//Remit Collection object for use on nodejs scripts
+var Collection = require('./src/collection');
+module.exports = Collection;
